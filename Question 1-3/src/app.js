cf0cffea30b91e6e189d7d5a553ec3dcf9d6867e
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-06-08 17:12:31 +0430

    added question 1

---
+++ Question 1-3/src/app.js
@@ -0,0 +1,10 @@
+const Rows = document.querySelectorAll("tr");
+Rows[1].className = "bg-default";
+Rows[2].className = "bg-primary";
+Rows[3].className = "bg-secondary";
+Rows[4].className = "bg-success";
+Rows[5].className = "bg-danger";
+Rows[6].className = "bg-warning";
+Rows[7].className = "bg-info";
+Rows[8].className = "bg-light";
+Rows[9].className = "bg-dark";
--- Question 1-3/tailwind.config.js
+++ Question 1-3/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: "#FFFFFF",
+        primary: "#DBEAFE",
+        secondary: "#F3E8FF",
+        success: "#DCFCE7",
+        danger: "#FEE2E2",
+        warning: "#FEF9C3",
+        info: "#E0E7FF",
+        light: "#F9FAFB",
+        dark: "#1F2937",
+      },
+    },
   },
   plugins: [],
 };
